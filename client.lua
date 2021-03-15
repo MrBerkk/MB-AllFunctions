0f5fe3b89cbf06e33ad7564a14b7f84b70667697
MrBerk <60743320+nova1238@users.noreply.github.com>
2021-03-15 16:49:55 +0300

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,405 @@
+-- OYUN ICERISINCE NPCLER YOKKEN CIKAN ORTAM SESLERINI KAPATIR
+
+CreateThread(function()   
+    StartAudioScene("CHARACTER_CHANGE_IN_SKY_SCENE");
+    SetAudioFlag("PoliceScannerDisabled",true);
+end)
+
+
+-- POLIS ARACLARININ SILAH VERMESINI ENGELLER
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(100)
+        id = PlayerId()
+        DisablePlayerVehicleRewards(id)
+    end
+end)
+
+
+--[[
+-- CROSSHAIR SILER
+
+Citizen.CreateThread(function()
+	local isSniper = false
+	while true do
+Citizen.Wait(0)
+	local ped = GetPlayerPed(-1)
+	local currentWeaponHash = GetSelectedPedWeapon(ped)
+	if currentWeaponHash == 100416529 then
+	isSniper = true
+	elseif currentWeaponHash == 205991906 then
+	isSniper = true
+ elseif currentWeaponHash == -952879014 then
+	isSniper = true
+ elseif currentWeaponHash == GetHashKey('WEAPON_HEAVYSNIPER_MK2') then
+	isSniper = true
+ else
+	isSniper = false
+		end
+			if not isSniper then
+			HideHudComponentThisFrame(14)
+		end
+	end
+end)
+]]
+
+-- MINI MAP ARAC DISI KAPANMA
+
+Citizen.CreateThread(function()
+    Citizen.Wait(10000)
+
+    while true do
+        local sleepThread = 500
+
+        local radarEnabled = IsRadarEnabled()
+
+        if not IsPedInAnyVehicle(PlayerPedId()) and radarEnabled then
+            DisplayRadar(false)
+        elseif IsPedInAnyVehicle(PlayerPedId()) and not radarEnabled then
+            DisplayRadar(true)
+        end
+
+        Citizen.Wait(sleepThread)
+    end
+end)
+
+
+-- NPCLERDEB YERE DUSEN SILAHLARI SILER
+Citizen.CreateThread(function()
+    while true do
+      Citizen.Wait(10000)
+      RemoveAllPickupsOfType(0xDF711959)
+      RemoveAllPickupsOfType(0xF9AFB48F)
+      RemoveAllPickupsOfType(0xA9355DCD)
+    end
+  end)
+
+  
+  -- PVP 
+  Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		SetCanAttackFriendly(GetPlayerPed(-1), true, false)
+		NetworkSetFriendlyFireOption(true)
+	end
+end)
+
+
+  -- PAUSE MENU NAME
+function AddTextEntry(key, value)
+	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
+end
+
+Citizen.CreateThread(function()
+  AddTextEntry('FE_THDR_GTAO', '~p~MB ~w~DEVELOPMENTS V2~y~/ ~b~https://discord.gg/DgcWYps7PF')
+end)
+
+
+  -- ACIL SERVISLERI KAPATIR
+
+Citizen.CreateThread(function()
+	while true do
+		Wait(1)
+		for i = 1, 12 do
+			EnableDispatchService(i, false)
+		end
+		SetPlayerWantedLevel(PlayerId(), 0, false)
+		SetPlayerWantedLevelNow(PlayerId(), false)
+		SetPlayerWantedLevelNoDrop(PlayerId(), 0, false)
+	end
+end)
+
+-- PD SILER 
+
+Citizen.CreateThread(function()
+    while true do
+    Citizen.Wait(100)
+    local playerPed = GetPlayerPed(-1)
+    local playerLocalisation = GetEntityCoords(playerPed)
+    ClearAreaOfCops(playerLocalisation.x, playerLocalisation.y, playerLocalisation.z, 400.0)
+    end
+    end)
+
+
+-- CTRL ÇÖMELME
+
+local crouched = false
+
+Citizen.CreateThread( function()
+    while true do 
+        Citizen.Wait( 1 )
+
+        local ped = GetPlayerPed( -1 )
+
+        if ( DoesEntityExist( ped ) and not IsEntityDead( ped ) ) then 
+            DisableControlAction( 0, 36, true ) -- INPUT_DUCK  
+
+            if ( not IsPauseMenuActive() ) then 
+                if ( IsDisabledControlJustPressed( 0, 36 ) ) then 
+                    RequestAnimSet( "move_ped_crouched" )
+
+                    while ( not HasAnimSetLoaded( "move_ped_crouched" ) ) do 
+                        Citizen.Wait( 100 )
+                    end 
+
+                    if ( crouched == true ) then 
+                        ResetPedMovementClipset( ped, 0 )
+                        crouched = false 
+                    elseif ( crouched == false ) then
+                        SetPedMovementClipset( ped, "move_ped_crouched", 0.25 )
+                        crouched = true 
+                    end 
+                end
+            end 
+        end 
+    end
+end )
+
+
+-- 'B' PARMAKLA GÖSTER
+
+local mp_pointing = false
+local keyPressed = false
+
+local function startPointing()
+    local ped = GetPlayerPed(-1)
+    RequestAnimDict("anim@mp_point")
+    while not HasAnimDictLoaded("anim@mp_point") do
+        Wait(1000)
+    end
+    SetPedCurrentWeaponVisible(ped, 0, 1, 1, 1)
+    SetPedConfigFlag(ped, 36, 1)
+    Citizen.InvokeNative(0x2D537BA194896636, ped, "task_mp_pointing", 0.5, 0, "anim@mp_point", 24)
+    RemoveAnimDict("anim@mp_point")
+end
+
+local function stopPointing()
+    local ped = GetPlayerPed(-1)
+    Citizen.InvokeNative(0xD01015C7316AE176, ped, "Stop")
+    if not IsPedInjured(ped) then
+        ClearPedSecondaryTask(ped)
+    end
+    if not IsPedInAnyVehicle(ped, 1) then
+        SetPedCurrentWeaponVisible(ped, 1, 1, 1, 1)
+    end
+    SetPedConfigFlag(ped, 36, 0)
+    ClearPedSecondaryTask(PlayerPedId())
+end
+
+local once = true
+local oldval = false
+local oldvalped = false
+
+Citizen.CreateThread(function()
+    while true do
+        Wait(1)
+
+        if once then
+            once = false
+        end
+
+        if not keyPressed then
+            if IsControlPressed(0, 29) and not mp_pointing and IsPedOnFoot(PlayerPedId()) then
+                Wait(200)
+                if not IsControlPressed(0, 29) then
+                    keyPressed = true
+                    startPointing()
+                    mp_pointing = true
+                else
+                    keyPressed = true
+                    while IsControlPressed(0, 29) do
+                        Wait(50)
+                    end
+                end
+            elseif (IsControlPressed(0, 29) and mp_pointing) or (not IsPedOnFoot(PlayerPedId()) and mp_pointing) then
+                keyPressed = true
+                mp_pointing = false
+                stopPointing()
+            end
+        end
+
+        if keyPressed then
+            if not IsControlPressed(0, 29) then
+                keyPressed = false
+            end
+        end
+        if Citizen.InvokeNative(0x921CE12C489C4C41, PlayerPedId()) and not mp_pointing then
+            stopPointing()
+        end
+        if Citizen.InvokeNative(0x921CE12C489C4C41, PlayerPedId()) then
+            if not IsPedOnFoot(PlayerPedId()) then
+                stopPointing()
+            else
+                local ped = GetPlayerPed(-1)
+                local camPitch = GetGameplayCamRelativePitch()
+                if camPitch < -70.0 then
+                    camPitch = -70.0
+                elseif camPitch > 42.0 then
+                    camPitch = 42.0
+                end
+                camPitch = (camPitch + 70.0) / 112.0
+
+                local camHeading = GetGameplayCamRelativeHeading()
+                local cosCamHeading = Cos(camHeading)
+                local sinCamHeading = Sin(camHeading)
+                if camHeading < -180.0 then
+                    camHeading = -180.0
+                elseif camHeading > 180.0 then
+                    camHeading = 180.0
+                end
+                camHeading = (camHeading + 180.0) / 360.0
+
+                local blocked = 0
+                local nn = 0
+
+                local coords = GetOffsetFromEntityInWorldCoords(ped, (cosCamHeading * -0.2) - (sinCamHeading * (0.4 * camHeading + 0.3)), (sinCamHeading * -0.2) + (cosCamHeading * (0.4 * camHeading + 0.3)), 0.6)
+                local ray = Cast_3dRayPointToPoint(coords.x, coords.y, coords.z - 0.2, coords.x, coords.y, coords.z + 0.2, 0.4, 95, ped, 7);
+                nn,blocked,coords,coords = GetRaycastResult(ray)
+
+                Citizen.InvokeNative(0xD5BB4025AE449A4E, ped, "Pitch", camPitch)
+                Citizen.InvokeNative(0xD5BB4025AE449A4E, ped, "Heading", camHeading * -1.0 + 1.0)
+                Citizen.InvokeNative(0xB0A6CFD2C69C1088, ped, "isBlocked", blocked)
+                Citizen.InvokeNative(0xB0A6CFD2C69C1088, ped, "isFirstPerson", Citizen.InvokeNative(0xEE778F8C7E1142E2, Citizen.InvokeNative(0x19CAFA3C87F7C2FF)) == 4)
+
+            end
+        end
+    end
+end)
+
+
+-- ARAC ITEKLEME
+
+ESX               = nil
+
+Citizen.CreateThread(function()
+  while ESX == nil do
+    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+    Citizen.Wait(1000)
+  end
+end)
+
+Config = {} 
+Config.DamageNeeded = 100.0 -- 100.0 being broken and 1000.0 being fixed a lower value than 100.0 will break it
+Config.MaxWidth = 5.0 -- Will complete soon
+Config.MaxHeight = 5.0
+Config.MaxLength = 5.0
+
+local Keys = {
+  ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
+  ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
+  ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
+  ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
+  ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
+  ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
+  ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
+  ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
+  ["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
+}
+
+local First = vector3(0.0, 0.0, 0.0)
+local Second = vector3(5.0, 5.0, 5.0)
+
+local Vehicle = {Coords = nil, Vehicle = nil, Dimension = nil, IsInFront = false, Distance = nil}
+Citizen.CreateThread(function()
+    Citizen.Wait(2000)
+    while true do
+        local ped = PlayerPedId()
+        local closestVehicle, Distance = ESX.Game.GetClosestVehicle()
+        local vehicleCoords = GetEntityCoords(closestVehicle)
+        local dimension = GetModelDimensions(GetEntityModel(closestVehicle), First, Second)
+        if Distance < 6.0  and not IsPedInAnyVehicle(ped, false) then
+            Vehicle.Coords = vehicleCoords
+            Vehicle.Dimensions = dimension
+            Vehicle.Vehicle = closestVehicle
+            Vehicle.Distance = Distance
+            if GetDistanceBetweenCoords(GetEntityCoords(closestVehicle) + GetEntityForwardVector(closestVehicle), GetEntityCoords(ped), true) > GetDistanceBetweenCoords(GetEntityCoords(closestVehicle) + GetEntityForwardVector(closestVehicle) * -1, GetEntityCoords(ped), true) then
+                Vehicle.IsInFront = false
+            else
+                Vehicle.IsInFront = true
+            end
+        else
+            Vehicle = {Coords = nil, Vehicle = nil, Dimensions = nil, IsInFront = false, Distance = nil}
+        end
+        Citizen.Wait(5000)
+    end
+end)
+
+
+Citizen.CreateThread(function()
+    while true do 
+        Citizen.Wait(5)
+        local ped = PlayerPedId()
+        if Vehicle.Vehicle ~= nil then
+ 
+                if IsVehicleSeatFree(Vehicle.Vehicle, -1) and GetVehicleEngineHealth(Vehicle.Vehicle) <= Config.DamageNeeded then
+                    ESX.Game.Utils.DrawText3D({x = Vehicle.Coords.x, y = Vehicle.Coords.y, z = Vehicle.Coords.z}, 'Press [~g~SHIFT~w~] and [~g~E~w~] to push the vehicle', 0.4)
+                end
+     
+
+            if IsControlPressed(0, Keys["LEFTSHIFT"]) and IsVehicleSeatFree(Vehicle.Vehicle, -1) and not IsEntityAttachedToEntity(ped, Vehicle.Vehicle) and IsControlJustPressed(0, Keys["E"])  and GetVehicleEngineHealth(Vehicle.Vehicle) <= Config.DamageNeeded then
+                NetworkRequestControlOfEntity(Vehicle.Vehicle)
+                local coords = GetEntityCoords(ped)
+                if Vehicle.IsInFront then    
+                    AttachEntityToEntity(PlayerPedId(), Vehicle.Vehicle, GetPedBoneIndex(6286), 0.0, Vehicle.Dimensions.y * -1 + 0.1 , Vehicle.Dimensions.z + 1.0, 0.0, 0.0, 180.0, 0.0, false, false, true, false, true)
+                else
+                    AttachEntityToEntity(PlayerPedId(), Vehicle.Vehicle, GetPedBoneIndex(6286), 0.0, Vehicle.Dimensions.y - 0.3, Vehicle.Dimensions.z  + 1.0, 0.0, 0.0, 0.0, 0.0, false, false, true, false, true)
+                end
+
+                ESX.Streaming.RequestAnimDict('missfinale_c2ig_11')
+                TaskPlayAnim(ped, 'missfinale_c2ig_11', 'pushcar_offcliff_m', 2.0, -8.0, -1, 35, 0, 0, 0, 0)
+                Citizen.Wait(2000)
+
+                local currentVehicle = Vehicle.Vehicle
+                 while true do
+                    Citizen.Wait(5)
+                    if IsDisabledControlPressed(0, Keys["A"]) then
+                        TaskVehicleTempAction(PlayerPedId(), currentVehicle, 11, 1000)
+                    end
+
+                    if IsDisabledControlPressed(0, Keys["D"]) then
+                        TaskVehicleTempAction(PlayerPedId(), currentVehicle, 10, 1000)
+                    end
+
+                    if Vehicle.IsInFront then
+                        SetVehicleForwardSpeed(currentVehicle, -1.0)
+                    else
+                        SetVehicleForwardSpeed(currentVehicle, 1.0)
+                    end
+
+                    if HasEntityCollidedWithAnything(currentVehicle) then
+                        SetVehicleOnGroundProperly(currentVehicle)
+                    end
+
+                    if not IsDisabledControlPressed(0, Keys["E"]) then
+                        DetachEntity(ped, false, false)
+                        StopAnimTask(ped, 'missfinale_c2ig_11', 'pushcar_offcliff_m', 2.0)
+                        FreezeEntityPosition(ped, false)
+                        break
+                    end
+                end
+            end
+        end
+    end
+end)
+
+
+-- NPC ARACLARI KITLER
+
+Citizen.CreateThread(function()
+    while true do
+        Wait(0)
+        local ped = GetPlayerPed(-1)
+        if DoesEntityExist(GetVehiclePedIsTryingToEnter(PlayerPedId(ped))) then
+            local veh = GetVehiclePedIsTryingToEnter(PlayerPedId(ped))
+            local lock = GetVehicleDoorLockStatus(veh)
+            if lock == 7 then
+                SetVehicleDoorsLocked(veh, 2)
+            end
+            local pedd = GetPedInVehicleSeat(veh, -1)
+            if pedd then
+                SetPedCanBeDraggedOut(pedd, false)
+            end
+        end
+    end
+ end)
